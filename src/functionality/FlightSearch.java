d50f26c9f5be0a7de0f10d15eb8004b01c9a0c98
gisli94 <Gisli@vr2t-dw505.rhi.hi.is>
2016-04-19 16:37:38 +0000

    Allskonar Flight

--- src/functionality/FlightSearch.java
+++ src/functionality/FlightSearch.java
@@ -15,16 +15,30 @@ public class FlightSearch {
     
     Flight[] currentFlights;
     
-    public FlightSearch(String departureLocation, String arrivalLocation, int noOfAdults, int noOfChildren, Date startTime, boolean flexDates) {
-        currentFlights = searchFlights(departureLocation, arrivalLocation, noOfAdults, noOfChildren, startTime, flexDates);
+    public FlightSearch(String departureLocation, String arrivalLocation, int noOfAdults, int noOfChildren, Date startTime, Date returnDate, boolean flexDates) {
+        currentFlights = searchFlights(departureLocation, arrivalLocation, noOfAdults, noOfChildren, startTime, returnDate, flexDates);
     }
     
-    private Flight[] searchFlights(String departureLocation, String arrivalLocation, int noOfAdults, int noOfChildren, Date startTime, boolean flexDates) {
+    private Flight[] searchFlights(String departureLocation, String arrivalLocation, int noOfAdults, int noOfChildren, Date startTime, Date returnDate, boolean flexDates) {
         functionality.flight.SearchController sc = new functionality.flight.SearchController();
         Flight[] fl = new Flight[0];
-        if (!flexDates) {
-            functionality.flight.Flight[] flights = sc.searchOne(departureLocation, arrivalLocation, noOfAdults, noOfChildren, startTime, flexDates);
-            fl = createFlightArray(flights);
+        if (returnDate == null) {
+            if (!flexDates) {
+                functionality.flight.Flight[] flights = sc.searchOne(departureLocation, arrivalLocation, noOfAdults, noOfChildren, startTime, flexDates);
+                fl = createFlightArray(flights);
+            }
+            else {
+                ////////////
+            }
+        }
+        else {
+            if (!flexDates) {
+                functionality.flight.Flight[][] flights = sc.searchBoth(departureLocation, arrivalLocation, noOfAdults, noOfChildren, startTime, returnDate, flexDates);
+                fl = createFlightArray(flights);
+            }
+            else {
+                ///////////
+            }
         }
         return fl;
     }
@@ -39,6 +53,29 @@ public class FlightSearch {
             flights[i] = new Flight(f[i].getDeparture(), f[i].getArrival(), f[i].getDateAndTime(), f[i].getAvailableSeats(), f[i].getCompany(), f[i].getStartPrice());
         }
         return flights;
+    }
+    
+    private Flight[][] createFlightArray(functionality.flight.Flight[][] f) {
+        Flight[] flights1 = new Flight[f[0].length];
+        Flight[] flights2 = new Flight[f[1].length];
+        for (int i = 0; i < flights1.length; i++) {
+            flights1[i] = new Flight(f[0][i].getDeparture(), f[0][i].getArrival(), f[0][i].getDateAndTime(), f[0][i].getAvailableSeats(), f[0][i].getCompany(), f[0][i].getStartPrice());
+        }
+        for (int i = 0; i < flights2.length; i++) {
+            flights2[i] = new Flight(f[1][i].getDeparture(), f[1][i].getArrival(), f[1][i].getDateAndTime(), f[1][i].getAvailableSeats(), f[1][i].getCompany(), f[1][i].getStartPrice());
+        }
+        return null;
+    }
+    
+    private Flight[] concat (Flight[] a, Flight[] b) {
+        Flight[] c = new Flight[a.length+b.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i];
+        }
+        for (int i = 0; i < a.length+b.length; i++) {
+            c[i-a.length] = b[i];
+        }
+        return c;
     }
     
 }
