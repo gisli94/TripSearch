7e2f12ef6af4376483d7b58fef2eaeefca10a8b6
gisli94 <Gisli@tg-dw222.rhi.hi.is>
2016-04-19 22:15:13 +0000

    dfg

--- src/functionality/TourSearch.java
+++ src/functionality/TourSearch.java
@@ -5,6 +5,11 @@
  */
 package functionality;
 
+import functionality.daytours.SearchCriteriaIsInvalidException;
+import java.util.ArrayList;
+import java.util.Date;
+import functionality.daytours.Trip;
+
 /**
  *
  * @author Gisli
@@ -13,12 +18,23 @@ public class TourSearch {
     
     private functionality.daytours.Trip[] currentTours;
     
-    public TourSearch(Date date, String type, String location, int numSeats) {
-        
+    public TourSearch(Date date, String type, String location, int numSeats) throws SearchCriteriaIsInvalidException {
+        currentTours = searchTours(date, type, location, numSeats);
     }
     
-    private searchTours(Date date, String type, String location, int numSeats) {
-        
+    private functionality.daytours.Trip[] searchTours(Date date, String type, String location, int numSeats) throws SearchCriteriaIsInvalidException {
+        functionality.daytours.SearchController sc = new functionality.daytours.SearchController();
+        int year = date.getYear();
+        int month = date.getMonth();
+        int day = date.getDate();
+        String str = ("" + day + "." + month+1 + "." + year);
+        functionality.daytours.SearchCriteria scrit = new functionality.daytours.SearchCriteria(str, type, location, numSeats);
+        ArrayList<functionality.daytours.Trip> al = new ArrayList<functionality.daytours.Trip>();
+        al = sc.searchForTrips(scrit);
+        for (int i = 0; i < al.size(); i++) {
+            
+        }
+        return null;
     }
     
     private functionality.daytours.Trip[] getCurrentTours() {
